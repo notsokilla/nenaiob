5477f3b61fb517a75d5627101ef80f56467310e7
notsokilla <glicinkz@gmail.com>
2025-11-12 01:35:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import hmac
 import sqlite3
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
-
 app = FastAPI()
 
 # Подключение к SQLite
